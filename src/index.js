bc6de81448c5a106a4a26907afd4b52a40cc470d
BMP29 <brenomp730@gmail.com>
2024-02-12 13:12:07 -0300

    loadProjForm function added and imported to index
    loadProjForm creates the form to the user write a name
    for the new project and append to the body

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import '../src/style.css'
 import './btnTheme'
 import './btnSideBar'
+import './loadProjForm'
+import loadProjForm from './loadProjForm';
+
+const btnAddProj = document.getElementById('btnAddProject');
+
+btnAddProj.addEventListener('click', loadProjForm);
 
 console.log("Everything is OK!")
