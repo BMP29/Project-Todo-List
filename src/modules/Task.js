7187f4046a7d6a17453cd5ae5bc39ffd70b92dc3
BMP29 <brenomp730@gmail.com>
2024-03-10 16:10:44 -0300

    task class now has a constructor

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,6 +1,13 @@
 class Task {
-    #title;
-    #description;
-    #date;
-    #priority;
+    title;
+    description;
+    date;
+    priority;
+
+    constructor(title, description, date, priority) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+    }
 }
