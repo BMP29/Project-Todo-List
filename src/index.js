772ba9cc71b764771bdf1ea761871a361d703b78
BMP29 <brenomp730@gmail.com>
2024-03-21 13:51:54 -0300

    functions imported from UIUX, IDs added to filters
    renderAllTasks and renderAllWithInInterval
    imported from UI-UX and bounded to the filters
    with eventListeners
    
    Sidebar filter's now has ids

--- src/index.js
+++ src/index.js
@@ -2,18 +2,38 @@ import '../src/style.css'
 import './btnTheme'
 import './btnSideBar'
 import { loadProjects, getAllProjects } from './modules/ProjectsController';
-import { loadProjForm, renderProjects, loadTaskForm } from './modules/UI-UX';
+import { loadProjForm, renderProjects, loadTaskForm, renderAllTasks, renderAllWithInInterval } from './modules/UI-UX';
 
 loadProjects();
 
 const btnAddProj = document.getElementById('btnAddProject');
 const btnAddTask = document.getElementById('btnAddTask');
+const btnInbox = document.getElementById('inbox');
+const btnToday = document.getElementById('today');
+const btnWeek = document.getElementById('week');
+const btnMonth = document.getElementById('month');
 
 btnAddProj.addEventListener('click', () => {
     loadProjForm();
 });
 
 btnAddTask.addEventListener('click', loadTaskForm);
+
+btnInbox.addEventListener('click', () => {
+    renderAllTasks();
+});
+
+btnToday.addEventListener('click', () => {
+    renderAllWithInInterval('today');
+});
+
+btnWeek.addEventListener('click', () => {
+    renderAllWithInInterval('week');
+});
+
+btnMonth.addEventListener('click', () => {
+    renderAllWithInInterval('month');
+});
 
 renderProjects(getAllProjects());
 
