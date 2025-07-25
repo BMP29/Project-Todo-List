b724c4e194edb054e8dba602460e547ef3624679
brenomp730@gmail.com <brenomp730@gmail.com>
2025-07-24 23:40:40 -0300

    refactor: Project and Task classes moved to new models folder
    Both Project and Task were out place in the modules folder. To better structure the project a models folder was created for classes, in which Project and Task files were moved to

---
+++ src/models/Project.js
@@ -0,0 +1,33 @@
+import Task from './Task' 
+
+class Project {
+    title;
+    todos;
+    constructor(title) {
+        this.title = title;
+        this.todos = [];
+    }
+
+    addTodo(title, description, date, important) { 
+        this.todos.push(new Task(title, description, date, important)); 
+    }
+    
+    removeTodo(index) { 
+        const toBeRemoved = this.todos[index];
+
+        const temp = this.todos.filter((todo) => todo !== toBeRemoved);
+        this.todos = temp;
+    }
+
+    getPositionOf(todo) {
+        let todoIndex;
+        this.todos.forEach((item, index) => {
+            if(item == todo) 
+                todoIndex = index;
+        });
+
+        return todoIndex;
+    }
+}
+
+export default Project;
